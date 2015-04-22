fc3b75bd49654e8f41ccf864ee480169e19a1e5d
senek <michal.sek@swmansion.com>
2015-04-22 18:47:56 +0200

    blog template light

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,7 @@ module PostsHelper
 	def all_posts?
 		( params[:controller] == 'posts' ) && ( params[:action] == 'all' )
 	end
+	def single_post?
+		( params[:controller] == 'posts' ) && ( params[:action] == 'show' )
+	end
 end
